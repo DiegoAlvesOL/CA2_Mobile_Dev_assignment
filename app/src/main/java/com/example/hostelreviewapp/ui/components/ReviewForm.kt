fd44a01c1d8ffe62cf55c5ec1541a3f4203cf0fd
Diego Alves de Oliveira <diego@Diegos-MacBook-Air-2.local>
2026-03-09 18:00:58 +0000

    I adjusted the email and Days Stayed fields so that there would be no line underlining the field as either selected or unselected.

--- app/src/main/java/com/example/hostelreviewapp/ui/components/ReviewForm.kt
+++ app/src/main/java/com/example/hostelreviewapp/ui/components/ReviewForm.kt
@@ -14,12 +14,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hostelreviewapp.model.Review
@@ -46,8 +48,14 @@ fun ReviewForm(){
         TextField(
             value = email,
             onValueChange = {newValue -> email = newValue},
-            shape = RoundedCornerShape(16.dp),
-            modifier = Modifier.fillMaxWidth()
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -56,7 +64,13 @@ fun ReviewForm(){
         TextField(
             value = daysStayed,
             onValueChange = { newValue -> daysStayed = newValue},
-            modifier = Modifier.fillMaxWidth()
+            shape =  RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
         )
 
         Spacer(modifier = Modifier.height(16.dp))
